70f5bb3a3aeb1c05d216fbe11e57769c84bc3ad9
radsylph <andresa@devtop.io>
2023-10-16 07:30:28 -0400

    feat: added new colors

--- constants/colors.js
+++ constants/colors.js
@@ -1,9 +1,22 @@
 const COLORS = {
-    black: "#222222",
-    primary : "#DFC592",
-    secundary : "#112E29",
-    terceary : "#E4EBEC",
-}
+  black: "#222222",
+  primary: "#DFC592",
+  secundary: "#112E29",
+  terceary: "#E4EBEC",
+  quaternary: "#005B41",
+  quientecary: "008170",
+  gray: "rgba(178,178,178,0.5)",
+  white: "#FFFFFF",
+  primaryText: "#232D3F",
+  secundaryText: "#0F0F0F",
+  tercearyText: "#FFFFFF",
+  error: "#FF0000",
+  warning: "#FFA500",
+  success: "#008000",
+  info: "#1E90FF",
+  background: "#F5F5F5",
+  border: "#CCCCCC",
+  disabled: "#CCCCCC",
+};
 
-
-export default COLORS
+export default COLORS;
