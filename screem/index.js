76f73573e4e5b73df9ab7787e2ebcfde0f2c84ac
radsylph <andresa@devtop.io>
2023-10-16 07:31:00 -0400

    feat: added reset password importation

--- screem/index.js
+++ screem/index.js
@@ -2,5 +2,6 @@ import Login from "./Login.jsx";
 import Welcome from "./Welcome.jsx";
 import SingUp from "./SingUp.jsx";
 import Main from "./Main.jsx";
+import ResetPassword from "./ResetPassword.jsx";
 
-export { Login, Welcome, SingUp, Main };
+export { Login, Welcome, SingUp, Main, ResetPassword };
