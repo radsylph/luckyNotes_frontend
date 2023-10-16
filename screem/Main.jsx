4074a47cfc2eada57cd58cc50f2341453cbbacbe
radsylph <andresa@devtop.io>
2023-10-16 07:32:10 -0400

    feat: added token  validation (sorta)

--- screem/Main.jsx
+++ screem/Main.jsx
@@ -5,46 +5,59 @@ import {
   ScrollView,
   TouchableOpacity,
   Pressable,
+  FlatList,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import COLORS from "../constants/colors";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import { Ionicons } from "@expo/vector-icons";
 import Checkbox from "expo-checkbox";
 import Button from "../components/Button";
+import SearchBar from "../components/SearchBar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Main = ({ navigation }) => {
-  const [isPasswordShown, setIsPasswordShown] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
+import { ItemNote, oneNote } from "../constants/ItemNote";
+
+const Main = () => {
+  const [token, setToken] = useState("");
+
+  const getToken = async () => {
+    try {
+      const tokenAuth = await AsyncStorage.getItem("token");
+      console.log(tokenAuth);
+      if (token !== null) {
+        setToken(tokenAuth);
+      }
+    } catch (error) {
+      console.log(error);
+      return alert(error);
+    }
+    console.log(token);
+  };
+
+  useEffect(() => {
+    getToken();
+  }, []);
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.secundary }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.primary }}>
+      <View style={{ marginHorizontal: 22 }}>
+        <View>
+          <SearchBar />
+        </View>
+      </View>
+
       <View style={{ flex: 1, marginHorizontal: 22 }}>
-        <ScrollView>
-          <View style={{ marginVertical: 22 }}>
-            <Button
-              title=" Out Session"
-              onPress={() => navigation.navigate("welcome")}
-              style={{
-                marginTop: 18,
-                marginBottom: 4,
-              }}
-            />
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "center",
-                marginVertical: 22,
-              }}
-            >
-              <Text style={{ fontSize: 16, color: COLORS.terceary }}>
-                {" "}
-                no hay nada aqui LARGO!!
-              </Text>
-            </View>
-          </View>
-        </ScrollView>
+        <FlatList
+          keyboardDismissMode="on-drag"
+          containerCustomStyle={{ overflow: "visible" }}
+          data={ItemNote}
+          renderItem={oneNote}
+          View
+        />
+
+        <Ionicons name="add-circle-outline" size={50} color={COLORS.terceary} />
       </View>
     </SafeAreaView>
   );
