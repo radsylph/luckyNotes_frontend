b9337c071199f735f76dc4278a142761d96b2596
radsylph <andresa@devtop.io>
2023-10-18 20:51:30 -0400

    feat: added functionality for log out

--- components/HAader.jsx
+++ components/HAader.jsx
@@ -1,75 +1,92 @@
 import {
-    View,
-    Text,
-    TextInput,
-    ScrollView,
-    TouchableOpacity,
-    Pressable,
-    FlatList,
-    Image,
-  } from "react-native";
-  import  { React,useEffect, useRef, useState } from "react";
-  import { SafeAreaView } from "react-native-safe-area-context";
-  import COLORS from "../constants/colors";
-  import { Ionicons } from "@expo/vector-icons";
-  import Button from "../components/Button";
-  import { NavigationContainer } from '@react-navigation/native';
-  import { createNativeStackNavigator } from '@react-navigation/native-stack';
-  import { ItemNote, oneNote} from "../constants/ItemNote";
-  import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-  
-  import BarButton from "../components/BarButton";
-  
-  
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+  Pressable,
+  FlatList,
+  Image,
+} from "react-native";
+import { React, useEffect, useRef, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import COLORS from "../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import Button from "../components/Button";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ItemNote, oneNote } from "../constants/ItemNote";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import BarButton from "../components/BarButton";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const HAader = ({ navigation }) => {
+  const deleteToken = async () => {
+    try {
+      await AsyncStorage.removeItem("token");
+      console.log("token removed");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const handleUserPress = () => {
-        alert("User");
-        navigation.navigate("user");
-      };
+  const handleUserPress = () => {
+    alert("User");
 
-      const handleCreatePress = () => {
-        alert("Create");
-        navigation.navigate("create");
-      };
+    navigation.navigate("user");
+  };
 
-      const handleLogOutPress = () => {
-        alert("welcome");
-        navigation.navigate("welcome");
-      };
+  const handleCreatePress = () => {
+    alert("Create");
+    navigation.navigate("create");
+  };
 
-    return( 
-<View style={{ backgroundColor: COLORS.primary, justifyContent: "space-between" ,flexDirection: "row",alignItems: "center",height:50, borderBottomColor:COLORS.terceary, borderBottomWidth: 1,}}>
+  const handleLogOutPress = () => {
+    deleteToken();
+    navigation.navigate("welcome");
+  };
 
-<TouchableOpacity onPress={handleLogOutPress}>
-<Ionicons style={{marginVertical:5}}
-name="log-out-outline" size={40}                
-color={COLORS.secundary}/>
-</TouchableOpacity>
+  return (
+    <View
+      style={{
+        backgroundColor: COLORS.primary,
+        justifyContent: "space-between",
+        flexDirection: "row",
+        alignItems: "center",
+        height: 50,
+        borderBottomColor: COLORS.terceary,
+        borderBottomWidth: 1,
+      }}
+    >
+      <TouchableOpacity onPress={handleLogOutPress}>
+        <Ionicons
+          style={{ marginVertical: 5 }}
+          name="log-out-outline"
+          size={40}
+          color={COLORS.secundary}
+        />
+      </TouchableOpacity>
 
- 
-<TouchableOpacity onPress={handleUserPress}>
-<Ionicons style={{marginVertical:5}}
-name="person-circle-outline" size={40}                
-color={COLORS.secundary}/>
-</TouchableOpacity>
-        
+      <TouchableOpacity onPress={handleUserPress}>
+        <Ionicons
+          style={{ marginVertical: 5 }}
+          name="person-circle-outline"
+          size={40}
+          color={COLORS.secundary}
+        />
+      </TouchableOpacity>
 
-<TouchableOpacity onPress={handleCreatePress}>
-<Ionicons style={{marginVertical:5}}
-name="add-circle" size={40}                
-color={COLORS.secundary}/>
-</TouchableOpacity>
-
-
-
-</View>
-          
-
-        );
-       
-
-}
+      <TouchableOpacity onPress={handleCreatePress}>
+        <Ionicons
+          style={{ marginVertical: 5 }}
+          name="add-circle"
+          size={40}
+          color={COLORS.secundary}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default HAader;
