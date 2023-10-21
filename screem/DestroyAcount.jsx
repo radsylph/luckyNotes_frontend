645162e6a56732fd5eb928b5c520635ff280b24e
radsylph <andresa@devtop.io>
2023-10-20 23:52:22 -0400

    feat: added destroy user functionality

--- screem/DestroyAcount.jsx
+++ screem/DestroyAcount.jsx
@@ -17,9 +17,47 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ItemNote, oneNote } from "../constants/ItemNote";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import HAader2 from "../components/HAader2";
 const DestroyAcount = ({ navigation }) => {
+  const deleteToken = async () => {
+    try {
+      await AsyncStorage.removeItem("token");
+      console.log("token removed");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const destroyUser = async () => {
+    try {
+      const tokenAuth = await AsyncStorage.getItem("token");
+      if (!tokenAuth) {
+        alert("please login again");
+        deleteToken();
+        navigation.navigate("welcome");
+      }
+      if (tokenAuth) {
+        deleteToken(); // delete token first
+        console.log(response);
+        alert("user deleted");
+        navigation.navigate("welcome");
+        const response = await axios.delete(
+          "https://luckynotesbackend-production.up.railway.app/auth/deleteUser",
+          {
+            headers: {
+              Authorization: `Bearer ${tokenAuth}`,
+            },
+          }
+        );
+      }
+    } catch (error) {
+      console.log(error.data);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.black }}>
       <HAader2 navigation={navigation} />
@@ -46,6 +84,7 @@ const DestroyAcount = ({ navigation }) => {
 
         <View style={{ width: "100%", paddingTop: 150 }}>
           <Button
+            onPress={() => destroyUser()}
             title="Delete acount"
             style={{
               borderColor: COLORS.terceary,
